d20c98e57a3b5dcd877ad0052984c94ff0708ae3
Nick Waters <nicholas.r.waters@gmail.com>
2023-10-27 10:28:30 -0400

    layout in html & setup in JS

---
+++ index.js
@@ -0,0 +1,18 @@
+const freelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
+
+let freelancerAd = document.createElement('div');
+let avgPrice = 
+freelancerAd.innerHTML = `The average starting price is ${freelancers.price}
+                          Available Freelancers
+                          ${freelancers.name} | ${freelancers.occupation} | ${freelancers.price}`;
+
+h1.appendChild(freelancerAd);
